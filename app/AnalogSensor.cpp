5467f4a598e45e65bd5bd973dd99835bcb748456
aarjunsrinivasan <arjunsrinivasan.a@gmail.com>
2020-10-05 00:01:26 -0400

    changed pointer to unique pointer to fix valgrind error 2

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,21 +1,31 @@
+/** 
+ *  @file    AnalogSensor.cpp
+ *  @author  Arjun Srinivasan Ambalam
+ *  @copyright GNU Public License
+ *  @brief Valgrind memory leak test
+ *  @section DESCRIPTION
+ *  Method definition
+ *  for the class AnalogSensor
+ */
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
+#include<memory>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+    : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+int AnalogSensor::Read() {
+    std::unique_ptr<std::vector<int>> readings = \
+    std::make_unique<std::vector<int>>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+
+    double result = std::accumulate(readings->begin(),
+        readings->end(), 0.0) / readings->size();
     return result;
 }
 
--- include/AnalogSensor.hpp
+++ include/AnalogSensor.hpp
@@ -1,13 +1,43 @@
-#pragma once
+/** 
+ *  @file    AnanlogSensor.hpp
+ *  @author  Arjun Srinivasan Ambalam
+ *  @copyright GNU Public License
+ *  @brief Valgrind Exerciise ENPM808X
+ *  @section DESCRIPTION
+ *  Class declaration
+ *	of the attributes of the class AnalogSensor
+ */
+#ifndef INCLUDE_ANALOGSENSOR_HPP_
+#define INCLUDE_ANALOGSENSOR_HPP_
 
 #include <iostream>
 
-class AnalogSensor
-{
-public:
-    AnalogSensor(unsigned int samples);
+/**
+ * @brief      This class describes an analog sensor.
+ */
+
+class AnalogSensor {
+ public:
+ 	   /**
+    * @brief constructor AnalogSensor class
+    * @param int Number of samples
+    * @return none
+    */
+    explicit AnalogSensor(unsigned int samples);
+/**
+*	@brief Destructor for the AnalogSensor class
+*	@param None
+*	@return None 
+*/
     ~AnalogSensor();
+/**
+*	@brief Read function to return the average  of class attributes
+*	@param Nome
+*	@return int Result
+*/
     int Read();
-private:
-    unsigned int mSamples;
+ private:
+    unsigned int mSamples;/*!< samples per milisecond*/
 };
+#endif  // INCLUDE_ANALOGSENSOR_HPP_
+
